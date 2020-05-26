58161c008f70209fc3e8203fc6ad2845e7102d1a
Teodora Petkova <tpetkova@IGE-XAO-BU.LOCAL>
2020-05-26 06:41:00 +0300

    added FFT 1D and 2D

--- Trails/FourierTransform.py
+++ Trails/FourierTransform.py
@@ -6,71 +6,131 @@ from skimage.io import imread
 """
 Discrete Fourier Transform 1D
 """
-def dft1D(x, sampling_points):
+
+
+def dft1D(x):
     N = len(x)
     X = np.array([])
-    for k in range(0, sampling_points): 
+    for k in range(0, N):
         Xk = np.complex(0, 0)
-        for n in range(0, N): # [0, N-1]
+        for n in range(0, N):  # [0, N-1]
             alpha = 2*math.pi*k*n/N
-            Xk +=  np.complex(x[n]) * np.complex(math.cos(alpha), (- math.sin(alpha)))
+            Xk += np.complex(x[n]) * \
+                np.complex(math.cos(alpha), (- math.sin(alpha)))
         X = np.append(X, [Xk])
     return X
+
 
 """
 Inverse Discrete Fourier Transform 1D
 """
-def idft1D(X, N): 
-    sampling_points = len(X)
+
+
+def idft1D(X):
+    N = len(X)
     x = np.array([])
-    for n in range(0, N): # [0, N-1]
+    for n in range(0, N):  # [0, N-1]
         xn = 0
-        for k in range(0, sampling_points): 
+        for k in range(0, N):
             alpha = 2*math.pi*k*n/N
             xn += X[k] * np.complex(math.cos(alpha), math.sin(alpha))
         x = np.append(x, [(1/N) * xn])
     return x
 
+
+"""
+Fast Fourier Transform
+"""
+
+
+def fft1D(x):
+    N = len(x)
+    # if N % 2 > 0:
+    #    raise ValueError("The input must be a power of 2")
+    if(N <= 1):
+        return x
+    M = (int)(N/2)
+    even = np.array(fft1D(x[0::2]))
+    odd = np.array(fft1D(x[1::2]))
+
+    frequency_bins = np.zeros(N, dtype=complex)
+    for k in range(0, M):
+        alpha = 2*math.pi*k/N
+        twiddle_factor = np.complex(math.cos(alpha), (- math.sin(alpha)))
+        oddTerm = twiddle_factor*odd[k]
+        frequency_bins[k] = even[k] + oddTerm
+        frequency_bins[k+M] = even[k] - oddTerm
+    return frequency_bins
+
+
+"""
+Inverse Fast Fourier Transform
+"""
+
+
+def ifft1D(freqs):
+    N = len(freqs)
+    if(N <= 1):
+        return freqs
+    M = (int)(N/2)
+    even = np.array(ifft1D(freqs[0::2]))
+    odd = np.array(ifft1D(freqs[1::2]))
+
+    x = np.zeros(N, dtype=complex)
+    for k in range(0, M):
+        alpha = 2*math.pi*k/N
+        twiddle_factor = np.complex(math.cos(alpha), math.sin(alpha))
+        oddTerm = twiddle_factor*odd[k]
+        x[k] = (1/2)*(even[k] + oddTerm)
+        x[k+M] = (1/2)*(even[k] - oddTerm)
+    return x
+
+
 """
 Extract sines from a frequency signal
 """
+
+
 def extract_sines(possible_frequencies, dft_frequencies):
     functions = np.array([])
     for (frequency, dft) in zip(possible_frequencies, dft_frequencies):
         amplitude = math.sqrt(dft.real**2 + dft.imag**2)
         if(amplitude > 0.000001):
-            phase = 0 #math.atan2(-dft.imag, dft.real) * np.pi #if dft.imag != 0 else 0
-            print(phase)
+            # math.atan2(-dft.imag, dft.real) * np.pi #if dft.imag != 0 else 0
+            phase = 0
+            # print(phase)
             # break the direct tie of the variables inside the main lambda
             # -> wrap them in another lambda called in the loop
-            sine = lambda a, p, f: (lambda t : ((1/a) * math.sin(p + f * 2 * math.pi * t)))
+            def sine(a, p, f): return (lambda t: (
+                (1/a) * math.sin(p + f*2*math.pi*t)))
             wrapped_sine = np.vectorize((sine)(amplitude, phase, frequency))
             functions = np.append(functions, [wrapped_sine])
     return functions
 
+
 def test_dft1D():
-    time_period = 1 # 1 second
-    Fs = 100.0;  # sampling rate - sampling rate is the number of samples per second
+    time_period = 1  # 1 second
+    Fs = 100.0  # sampling rate - sampling rate is the number of samples per second
     # it is the reciprocal of the sampling time, i.e. 1/T, also called the sampling frequency
 
-    Ts = time_period/Fs; # sampling interval / sampling period
+    Ts = time_period/Fs  # sampling interval / sampling period
     # the sampling time is the time interval between successive samples, also called the sampling interval or the sampling period
 
-    t = np.arange(0, time_period, Ts) # time vector in seconds
+    t = np.arange(0, time_period, Ts)  # time vector in seconds
     N = t.size
-    f = np.linspace(0, 1/Ts, N) # frequency vector - 1 / sampling_interval
+    f = np.linspace(0, 1/Ts, N)  # frequency vector - 1 / sampling_interval
 
     cycle = 2*np.pi
-    frequency1 = 1 * cycle # frequency of the signal 1 in Hertz
-    frequency2 = 3 * cycle # frequency of the signal 3 in Hertz
+    frequency1 = 1 * cycle  # frequency of the signal 1 in Hertz
+    frequency2 = 3 * cycle  # frequency of the signal 3 in Hertz
 
     y = np.sin(t*frequency1) + np.sin(t*frequency2)
-    dft = dft1D(y, N)
+    dft = dft1D(y)
     #fft = np.fft.fft(y, N)
-    idft = idft1D(dft, N)
+    idft = idft1D(dft)
 
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
-    #ax1.xlabel("Amplitude")
+    # ax1.xlabel("Amplitude")
     #ax1.ylabel("Time [s]")
     ax1.plot(t, y)
     #ax2.xticks([np.arange(0, 1, 1)])
@@ -80,14 +140,7 @@ def test_dft1D():
         ax3.plot(t, sine(t))
     ax4.plot(t, idft)
     plt.show()
- 
-def check_correctness_1D():
-    N = 100
-    x = np.random.random(N)
-    dft = dft1D(x, N)
-    fft = np.fft.fft(x)
-    print(np.allclose(dft, fft))
-    print(np.allclose(idft1D(dft, N), np.fft.ifft(fft)))
+
 
 """
 Discrete Fourier Transform 2D
@@ -99,69 +152,101 @@ the horizontal axis is frequency f and the vertical axis is space y
 the second step is to apply 1D Fourier transform individually to 
 the vertical line of the intermediate image
 """
-def dft2D(image):
+
+
+def ft2D(image, ft):
     M, N = image.shape
-    frequency_image = np.empty((M, N), dtype=complex)
-    
+    transformed_image = np.empty((M, N), dtype=complex)
+
     # DFT 1D on rows
     for m in range(0, M):
-        frequency_image[m, :] = dft1D(image[m, :], N)
-    
+        transformed_image[m, :] = ft(image[m, :])
+
     # DFT 1D on columns
     for n in range(0, N):
-        frequency_image[:, n] = dft1D(frequency_image[:, n], M)
-        
-    return frequency_image
+        transformed_image[:, n] = ft(transformed_image[:, n])
 
-"""
-Inverse Discrete Fourier Transform 2D
-"""
-def idft2D(frequency_image):
-    M, N = frequency_image.shape
-    image = np.empty((M, N), dtype=complex)
-    
-    # IDFT 1D on rows
-    for m in range(0, M):
-        image[m, :] = idft1D(frequency_image[m, :], N)
-    
-    # IDFT 1D on columns
-    for n in range(0, N):
-        image[:, n] = idft1D(image[:, n], M)
-        
-    return image
+    return transformed_image
+
+
+def dft2D(image):
+    return ft2D(image, dft1D)
+
+
+def idft2D(image):
+    return ft2D(image, idft1D)
+
+
+def fft2D(image):
+    return ft2D(image, fft1D)
+
+
+def ifft2D(image):
+    return ft2D(image, ifft1D)
+
 
 def display(image):
-        # if there is only one channel to show, display it as grayscale",
-        cm = None
-        if(len(image.shape)) == 2:
-            cm = "gray"
-        plt.figure(figsize = (5, 10))
-        plt.imshow(image, cmap = cm)
-        plt.xticks([])
-        plt.yticks([])
-        plt.show()
+    # if there is only one channel to show, display it as grayscale",
+    cm = None
+    if(len(image.shape)) == 2:
+        cm = "gray"
+    plt.figure(figsize=(5, 10))
+    plt.imshow(image, cmap=cm)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
 
 def test_dft2D_with_small_image_pulse():
     original_image = np.array(
-        [[0,0,0,0],
-         [0,1,1,0],
-         [0,1,1,0],
-         [0,0,0,0]])
-    
+        [[0, 0, 0, 0],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [0, 0, 0, 0]])
+
     display(original_image)
     dft = dft2D(original_image)
     display(np.abs(dft))
     idft = idft2D(dft)
     display(np.abs(idft))
 
-def check_correctness_2D():
-    m = np.random.rand(30, 30)
-    dft = dft2D(m)
-    fft = np.fft.fft2(m)
-    print(np.allclose(dft, fft))
-    print(np.allclose(idft2D(dft), np.fft.ifft2(dft)))
+def test_fft2D_with_photo():
+    original_image = imread("https://www.hlevkin.com/TestImages/cameraman.bmp")
+    display(original_image[:, :])
+    def shiftlog(m): return np.fft.fftshift(np.log(1+np.abs(m)))
+    fft1 = fft2D(original_image[:, :])
+    display(shiftlog(fft1))
+    ifft = np.fft.ifft2(fft1)
+    display(np.abs(ifft))
 
-test_dft1D()
+
+def check_correctness_1D():
+    N = 32
+    x = np.random.random(N)
+    dft = dft1D(x)
+    fft = np.fft.fft(x)
+    custom_fft = fft1D(x)
+    ifft = np.fft.ifft(fft)
+    print("DFT 1D: ", np.allclose(dft, fft))
+    print("Inverse DFT 1D: ", np.allclose(idft1D(dft), ifft))
+    print("FFT 1D: ", np.allclose(custom_fft, fft))
+    print("Inverse FFT 1D: ", np.allclose(ifft1D(custom_fft), ifft))
+
+
+def check_correctness_2D():
+    m = np.random.rand(32, 32)
+    dft = dft2D(m)
+    custom_fft = fft2D(m)
+    fft = np.fft.fft2(m)
+    ifft = np.fft.ifft2(dft)
+    print("DFT 2D:", np.allclose(dft, fft))
+    print("IDFT 2D: ", np.allclose(idft2D(dft), ifft))
+    print("FFT 2D: ", np.allclose(custom_fft, fft))
+    print("IFFT 2D: ", np.allclose(ifft2D(custom_fft), ifft))
+
+
 check_correctness_1D()
-test_dft2D_with_small_image_pulse()
 check_correctness_2D()
+test_dft1D()
+test_dft2D_with_small_image_pulse()
+test_fft2D_with_photo()
